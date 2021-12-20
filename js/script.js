aa67229e4f3c0464849d8925644067046697d249
IskahnX <davidecroce10@gmail.com>
2021-12-20 15:33:50 +0100

    list of things to do

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log ('JS OK')
+
+/*
+
+1 - Chiedere Nome
+
+2 - Chiedere Cognome 
+
+3 - Chiedere Colore Preferito
+
+4 - Far uscire fuori la password formata da nome, cognome. colore preferito + il numero 21
+
+*/
+
