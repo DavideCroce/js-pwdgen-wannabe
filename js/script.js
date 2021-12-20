352e69c1bed8c2eff726126045e7ed422442fc91
IskahnX <davidecroce10@gmail.com>
2021-12-20 15:45:56 +0100

    Finished work ... added name, surname and color

--- js/script.js
+++ js/script.js
@@ -12,3 +12,26 @@ console.log ('JS OK')
 
 */
 
+
+/* Ask the Name */
+
+const userName = prompt ('Qual è il tuo nome?');
+console.log (userName);
+
+/* Ask the SurName */
+
+const userSurname = prompt ('Qual è il tuo cognome?');
+console.log (userSurname);
+
+/* Ask the Favourite Color */
+
+const FavColor = prompt ('Qual è il tuo colore preferito?');
+console.log (FavColor);
+
+/* Re-Take the Element */
+
+const resultElement = document.getElementById ('password');
+
+/* Result to the Password */
+
+resultElement.innerText = 'La tua password è:' + userName + userSurname + FavColor + '21';
